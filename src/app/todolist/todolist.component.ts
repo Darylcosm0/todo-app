14ed94e26484521b90fbadee1a81d47dfb224787
Darylcosm0 <mutaidaryl@gmail.com>
2023-08-28 16:26:36 +0300

    ✨ Added stylish UI for task app

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-  taskArray = [{ taskName: 'Brush teeth', isCompleted: false }];
+  taskArray = [{ taskName: 'fuck around', isCompleted: false }];
 
   constructor() { }
 
